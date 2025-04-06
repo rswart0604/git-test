e5058b2de7cf002f2b03379b2d22d0e1c7f1e067
rswart0604 <rswart0604@gmail.com>
2025-04-06 00:02:18 -0700

    Made some formatting changes to IVP

--- IVP.py
+++ IVP.py
@@ -13,16 +13,16 @@ def forward_euler(h, t0, y0, tf):
 def rk2(h, t0, y0, tf):
     while t0 < tf:
         k1 = h * f(y0)
-        k2 = h * f(y0+k1)
-        y0 = y0 + 0.5 (k1 + k2)
+        k2 = h * f(y0+(2/3)*k1)
+        y0 = y0 + (0.25*k1 + 0.75*k2)
     return y0
 
 def rk4(h, t0, y0, tf):
     while t0 < tf:
         k1 = h * f(y0)
-        k2 = h * f(y0+0.5*k1)
-        k3 = h * f(y0+0.5*k2)
+        k2 = h * f(y0 + 0.5*k1)
+        k3 = h * f(y0 + 0.5*k2)
         k4 = h * f(y0 + k3)
-        y0 = y0 + (1/6)*(k1+2*k2+2*k3+k4)
+        y0 = y0 + (1/6) * (k1 + 2*k2 + 2*k3 + k4)
     return y0
 
