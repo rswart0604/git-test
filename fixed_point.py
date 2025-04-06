acc8ac5aaca4ebfae2e0332cdaaabe4eee8c7253
rswart0604 <rswart0604@gmail.com>
2025-04-06 12:21:11 -0700

    Added fixed_point.py

---
+++ fixed_point.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def f(x):
+    return np.square(x) - 4*x - 3
+
+def g(x):
+    return np.sqrt(4*x+3)
+
+def fixed_point(x0):
+    while f(x0) > 1e-3:
+        x0 = g(x0)
+    return x0
+:wq
