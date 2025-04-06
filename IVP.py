160c6a9a32ab8f7ae60533b5a7e3aba185ca9c5c
rswart0604 <rswart0604@gmail.com>
2025-04-06 13:08:45 -0700

    Added backward euler

--- IVP.py
+++ IVP.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def f(y=None,t=None):
-    return np.square(y)
+    return 0.5*t
 
 def forward_euler(h, t0, y0, tf):
     while t0 < tf
@@ -24,5 +24,11 @@ def rk4(h, t0, y0, tf):
         k3 = h * f(y0 + 0.5*k2)
         k4 = h * f(y0 + k3)
         y0 = y0 + (1/6) * (k1 + 2*k2 + 2*k3 + k4)
+    return y0
+
+def backward_euler(h, t0, y0, tf):
+    while t0 < tf:
+        y0 = y0 + h*f(y0, t0+h)
+        t0 += h
     return y0
 
