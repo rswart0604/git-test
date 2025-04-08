db2acd9dc4c1d3aee9c152d6cd36aaa0e76b9215
rswart0604 <rswart0604@gmail.com>
2025-04-07 17:49:05 -0700

    Added tests to fixed_point

--- fixed_point.py
+++ fixed_point.py
@@ -1,13 +1,16 @@
 import numpy as np
 
 def f(x):
-    return np.square(x) - 4*x - 3
+    return np.square(x) - 7*x + 12
 
 def g(x):
-    return np.sqrt(4*x+3)
+    return np.sqrt(7*x - 12)
 
 def fixed_point(x0):
-    while f(x0) > 1e-3:
+    while np.abs(f(x0)) > 1e-6:
         x0 = g(x0)
     return x0
 
+x0 = 4.6
+print(f"{fixed_point(x0)}")
+print(f"Real root is 4")
