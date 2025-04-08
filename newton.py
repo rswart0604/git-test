434f6f72a78de5e2d4e94ee50b8d1d74e8fbf8ca
rswart0604 <rswart0604@gmail.com>
2025-04-07 17:51:28 -0700

    Add testing to newton

--- newton.py
+++ newton.py
@@ -1,13 +1,16 @@
 import numpy as np
 
 def f(x):
-    return np.square(x)-4*x-10
+    return np.square(x) - 7.0*x + 12.0
 
 def g(x):
-    return 2*x-4
+    return 2.0*x - 7.0
 
 def newton(x0):
-    while f(x0) > 1e-3:
-        x0 = x0 - g(x0)/f(x0)
+    while np.abs(f(x0)) > 1e-6:
+        x0 = x0 - f(x0)/g(x0)
     return x0
 
+x0 = 5.0
+print(f'{newton(x0)}')
+print('Real root is 4')
